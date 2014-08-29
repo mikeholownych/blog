175cac90f26eef1fc413cd420bdd842a1e87826c
Mike Holownych <mike.holownych@gmail.com>
2014-08-29 19:53:45 +0000

    finished blog comments feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Blog::Application.routes.draw do
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
+
   root to: 'articles#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140829162005) do
+ActiveRecord::Schema.define(version: 20140829193055) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,16 @@ ActiveRecord::Schema.define(version: 20140829162005) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "comments", force: true do |t|
+    t.string   "author_name"
+    t.text     "body"
+    t.integer  "article_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["article_id"], name: "index_comments_on_article_id", using: :btree
 
   create_table "posts", force: true do |t|
     t.string   "title"
